49efef8102702eff275ff72a83ae5203fc0e8824
Haleigh Rzonca <hgielah@gmail.com>
2022-08-29 18:12:04 -0700

    notes to what i need to do.

--- script.js
+++ script.js
@@ -10,5 +10,17 @@ function writePassword() {
 
 }
 
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+//TODO: click button to generate passward. 
+//TODO: set criteria for passward you want to generate
+//TODO: be able to select the different criteria
+//TODO: be able to pick the length of the passward (longer than 8 but not longer than 128)
+//TODO: include lowercase, uppercase, numeric, and special charaters
+//TODO: validation page before generating
+//TODO: generated passward matches expected criteria
+//TODO: passward is displayed 
