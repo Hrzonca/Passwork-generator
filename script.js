3e3de833f63048407826ab66f5414e847151abf1
Haleigh Rzonca <hgielah@gmail.com>
2022-08-30 14:34:46 -0700

    Made some arrays for elements i need, have an overall base to what prompts need to happen when to generate a passward

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 // Assignment Code
+//TODO: include lowercase, uppercase, numeric, and special charaters
+const number = ['1','2','3','4','5','6','7','8','9','0']
+const specCharater = ['%','!','#','$','&','+','!','?']
+const upperLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+const lowerLetter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
+Array.from({length: 128}, (_, i) => i + 1)
+//=> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 var generateBtn = document.querySelector("#generate");
+
+//TODO: click button to generate passward. 
+function generatePassword(){
+    //making sure that stuff is actually working, check console
+    console.log("Hey! you clicked the button")
+//1. propmt user
+//passward length between 8and 128
+//propmt user number
+//propmt user specCharater
+//propmt user uppercase
+//propmt user lowercase
+//2. validate input
+//3. generate based on criteria
+//4. display passward
+
+    return "Generated password will go here"
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -13,14 +38,25 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+    
+
+ 
 
 
 
-//TODO: click button to generate passward. 
-//TODO: set criteria for passward you want to generate
-//TODO: be able to select the different criteria
-//TODO: be able to pick the length of the passward (longer than 8 but not longer than 128)
-//TODO: include lowercase, uppercase, numeric, and special charaters
-//TODO: validation page before generating
-//TODO: generated passward matches expected criteria
-//TODO: passward is displayed 
+//Criteria for project
+//GIVEN I need a new, secure password
+//WHEN I click the button to generate a password
+//THEN I am presented with a series of prompts for password criteria
+//WHEN prompted for password criteria
+//THEN I select which criteria to include in the password
+//WHEN prompted for the length of the password
+//THEN I choose a length of at least 8 characters and no more than 128 characters
+//WHEN asked for character types to include in the password
+//THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+//WHEN I answer each prompt
+//THEN my input should be validated and at least one character type should be selected
+//WHEN all prompts are answered
+//THEN a password is generated that matches the selected criteria
+//WHEN the password is generated
+//THEN the password is either displayed in an alert or written to the page
