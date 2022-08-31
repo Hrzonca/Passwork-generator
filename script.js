191917ef06050e570890766e6e8960ec4ef09a9d
Haleigh Rzonca <hgielah@gmail.com>
2022-08-30 19:03:43 -0700

    computer registers all inputs except the password length.

--- script.js
+++ script.js
@@ -1,62 +1,67 @@
 // Assignment Code
 //TODO: include lowercase, uppercase, numeric, and special charaters
-const number = ['1','2','3','4','5','6','7','8','9','0']
-const specCharater = ['%','!','#','$','&','+','!','?']
-const upperLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-const lowerLetter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
-Array.from({length: 128}, (_, i) => i + 1)
-//=> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const Number = ['1','2','3','4','5','6','7','8','9','0']
+const SpecCharater = ['%','!','#','$','&','+','!','?', '(', ')']
+const UpperLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+const LowerLetter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
+
+let charSet = []
+let generatedPassword = []
+//problems: only displaying 72 so the user input isnt working or linked to something. password length not showing up in console log. password not generating in box
+
 
 var generateBtn = document.querySelector("#generate");
 
 //TODO: click button to generate passward. 
-function generatePassword(){
-    //making sure that stuff is actually working, check console
-    console.log("Hey! you clicked the button")
-//1. propmt user
-//passward length between 8and 128
-//propmt user number
-//propmt user specCharater
-//propmt user uppercase
-//propmt user lowercase
-//2. validate input
-//3. generate based on criteria
-//4. display passward
-
-    return "Generated password will go here"
-}
-
-// Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    charSet =[];
+//1. propmt user
 
-  passwordText.value = password;
+let passwordLength = prompt('How long do you want your passward? Choose between 8 and 128 charaters'); {
+    if (passwordLength <= 8 || passwordLength >= 128) {
+        window.alert("Invalid entry, please try again");
+    } else { 
+let number = confirm('Do you want numbers in your passward?'); {
+        if (number) {
+            charSet.push.apply(charSet, Number);
+            console.log("Number applied");
+        } 
+let specCharater = confirm('Do you want special charaters?'); {
+    if (specCharater) {
+        charSet.push.apply(charSet, SpecCharater);
+        console.log("specCharater applied");
+    }
+let upperLetters = confirm('Do you want uppercase letters in your passward?');
+    if (upperLetters) {
+    charSet.push.apply(charSet, UpperLetters);
+    console.log("upperletters applied");
 
+let lowerLetter = confirm('Do you want lowercase letters in your passward?');
+    if (lowerLetter) {
+    charSet.push.apply(charSet, LowerLetter);
+    console.log("lowerletters applied");
+    return "password goes here"
 }
+}
+    console.log(charSet)
 
+    
+}
+}}}}
+// Write password to the #password input
+function generatePassword() {
+    var password = generatePassword();
+var passwordText = document.querySelector("#password");
+
+passwordText.value = password;
+    for (let i = 0; i < passwordLength; i++) {
+        var index = Math.floor(Math.random() * charSet.passwordLength)
+        generatedPassword[i] = charSet[index]
+    }
+
+    
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
     
-
- 
-
-
-
-//Criteria for project
-//GIVEN I need a new, secure password
-//WHEN I click the button to generate a password
-//THEN I am presented with a series of prompts for password criteria
-//WHEN prompted for password criteria
-//THEN I select which criteria to include in the password
-//WHEN prompted for the length of the password
-//THEN I choose a length of at least 8 characters and no more than 128 characters
-//WHEN asked for character types to include in the password
-//THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-//WHEN I answer each prompt
-//THEN my input should be validated and at least one character type should be selected
-//WHEN all prompts are answered
-//THEN a password is generated that matches the selected criteria
-//WHEN the password is generated
-//THEN the password is either displayed in an alert or written to the page
